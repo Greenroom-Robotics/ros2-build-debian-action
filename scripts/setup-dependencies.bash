a6104422a5774c18a4f76ea2ba2afd4fe50ff4a6
David Revay <daverevay@gmail.com>
2022-05-26 11:57:28 +1000

    feat: use deps from the docker container

--- scripts/setup-dependencies.bash
+++ scripts/setup-dependencies.bash
@@ -1,14 +1,7 @@
 ROS2_DISTRO="$1"
 
-# Install required tools
-sudo apt update && sudo apt install -y \
-  debhelper \
-  dh-python \
-  python3-bloom \
-  python3-catkin-pkg \
-  python3-colcon-common-extensions \
-  fakeroot || exit $?
 
 # Initialize and update rosdep
+sudo apt update
 sudo rosdep init || true
 rosdep update || exit $?
