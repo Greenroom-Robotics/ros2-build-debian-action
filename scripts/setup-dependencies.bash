dea09d4df1df771656cdff4a744a55f8af763a26
David Revay <daverevay@gmail.com>
2022-05-26 11:41:31 +1000

    feat: don't install rosdep, use the one from docker

--- scripts/setup-dependencies.bash
+++ scripts/setup-dependencies.bash
@@ -1,11 +1,5 @@
 ROS2_DISTRO="$1"
 
-# Add ROS 2 GPG key
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg || exit $?
-
-# Add ROS 2 repository to the source list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null || exit $?
-
 # Install required tools
 sudo apt update && sudo apt install -y \
   debhelper \
@@ -13,7 +7,6 @@ sudo apt update && sudo apt install -y \
   python3-bloom \
   python3-catkin-pkg \
   python3-colcon-common-extensions \
-  python3-rosdep \
   fakeroot || exit $?
 
 # Initialize and update rosdep
